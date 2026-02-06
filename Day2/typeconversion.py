9808a27272a704f07ac609173af092101ac5511d
Harshalwath <harshalwath27@gmail.com>
2026-02-06 11:21:28 +0530

    follow

--- Day2/typeconversion.py
+++ Day2/typeconversion.py
@@ -15,7 +15,8 @@ a = int("2")
 b = 4.25
 
 print(a+b)
- #how find typ
+
+#how find typ
 
 a = "harsh"
 print(type(a))
--- practicequestion/code1.py
+++ practicequestion/code1.py
@@ -1,3 +1,5 @@
+#WAP to input 2 number &print their sum
+
 num1 = int(input("enter a number"))
 num2 = int(input("enter a number"))
 
