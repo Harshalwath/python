55670fbe112e7f6e1257bd079e23b5b854c595c5
Harshalwath <harshalwath27@gmail.com>
2026-02-06 11:19:11 +0530

    error solve

---
+++ Day2/typeconversion.py
@@ -0,0 +1,21 @@
+#type conversion
+
+a  = 2
+b = 4.25
+
+sum = a + b
+print(sum)
+
+#type casting 
+#convert data typ into another typ
+
+#type casting
+
+a = int("2")
+b = 4.25
+
+print(a+b)
+ #how find typ
+
+a = "harsh"
+print(type(a))
---
+++ practicequestion/code2.py
@@ -0,0 +1,7 @@
+# WAP to input side of a square & print its area
+
+side = int(input("enter side of square"))
+
+area = (side*side)
+
+print(area)
