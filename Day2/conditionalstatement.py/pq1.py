515d851f86314e801186cad7aa2135aa5b46e068
Harshalwath <harshalwath27@gmail.com>
2026-02-25 23:56:15 +0530

    initial

---
+++ Day2/conditionalstatement.py/pq1.py
@@ -0,0 +1,23 @@
+# WAP FOR grade student based on marks
+marks = int(input("enter marks:"))
+
+if(marks >= 90):
+    print("congratulations")
+    print("grade A")
+    print("outstanding")
+
+
+elif(90> marks >= 80):
+     print("grade B")
+     print("excellent")
+
+elif(80> marks >= 70):
+    print("grade C")
+    (print(good))
+
+elif(70> marks >=60):
+    print("grade D")
+    print("need to improve")
+
+else:
+    print("improve your study")
