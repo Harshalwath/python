cac220c4f3ec5a2f115ef62da155717351c6a831
Harshalwath <harshalwath27@gmail.com>
2026-02-26 09:26:32 +0530

    simple calci

---
+++ Day2/conditionalstatement.py/pq5.py
@@ -0,0 +1,38 @@
+# Simple Calculator
+
+num1 = float(input("enter first number:"))
+
+op= input(" enter operator")
+if(1.):
+    print("+")
+
+elif(2.):
+    print("-")
+
+elif(3.):
+    print("*")
+
+elif(4.):
+    print("/")
+
+else:
+    print("invalid")
+
+
+
+num2 = float(input("enter second number:"))
+
+if(1.):
+    print("Result=", num1 + num2)
+
+elif(2.):
+    print("result", num1 - num2)
+
+elif(3.):
+    print("result", num1 * num2)
+
+elif(4.):
+    print("result", num1 / num2)
+
+else:
+    print("invalid")
