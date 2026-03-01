5b35add2ffd59b43532f83a054259766d7a9759d
Harshalwath <harshalwath27@gmail.com>
2026-03-01 15:32:48 +0530

    list

---
+++ Day2/list&tuple.py/pq2.py
@@ -0,0 +1,14 @@
+# take a input from user and store in list and print it
+num1 = int(input("Enter a first number:"))
+
+num2 = int(input("Enter a second number:"))
+
+num3 = int(input("Enter a third number:"))
+
+num4 = int(input("Enter a fourth number:"))
+
+num5 = int(input("Enter a fifth number:"))
+
+numbers = [num1,num2,num3,num4,num5]
+
+print("list:",numbers)
