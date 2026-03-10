4f7043cd7e1ec6971f153d2a25910f791fefeb10
Harshalwath <harshalwath27@gmail.com>
2026-03-10 23:14:13 +0530

    commit

---
+++ Day2/miniproject.py/1.py
@@ -0,0 +1,11 @@
+#band name genrator 
+print("welcome to the Band Name Generator.")
+
+city = input("Which city are you form :?")
+
+pet = input("What is the your pet name?")
+
+bandname = (city+ " " + pet )
+
+print("your band name is:",bandname)
+
