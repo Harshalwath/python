8be0ab6a5e49ac3bf0fd27fed1f559914d052331
Harshalwath <harshalwath27@gmail.com>
2026-03-15 23:36:37 +0530

    commit

--- Day2/list&tuple.py/pq9.py
+++ Day2/list&tuple.py/pq9.py
@@ -1,4 +1,5 @@
 # wap to ask the user to enter names of their 3 fav movies name &  store them in list.
+movies = []
 
 movie1 = str(input("Enter the name first movie:"))
 
@@ -6,6 +7,8 @@ movie2 = str(input("Enter the name of second movie:"))
 
 movie3 = str(input("Enter the name of third movie:"))
 
-list = [movie1,movie2,movie3]
+movies.append(movie1)
+movies.append(movie2)
+movies.append(movie3)
 
-print(list)
+print(movies)
