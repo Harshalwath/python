0dc52500157c73fca4cfb46d90455a5788be6fb5
Harshalwath <harshalwath27@gmail.com>
2026-03-16 23:07:16 +0530

    commit

---
+++ Day2/list&tuple.py/pq11.py
@@ -0,0 +1,11 @@
+#WAP to count the number of students with the “A” grade in the following tuple
+#["C","d","a","a","b","b","a"]
+
+grade=["C","D","A","B","B","A"]
+
+print(grade.count("A"))
+
+print(grade.sort())
+
+print(grade)
+
