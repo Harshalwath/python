65a7fd46df0a70fbc6aac3f2da27e6273c8e0881
Harshalwath <harshalwath27@gmail.com>
2026-03-16 22:35:57 +0530

    commit

---
+++ Day2/list&tuple.py/pq10.py
@@ -0,0 +1,12 @@
+#WAP to check if a list contains a palindrome of elements.
+
+list1 =["1","2","1"]
+
+copy_list1=list1.copy()
+copy_list1.reverse()
+
+if(copy_list1==list1):
+    print("palindrome")
+
+else:
+    print("not palindrome")
