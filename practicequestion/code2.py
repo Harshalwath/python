c886a5fb9da302024602bec19c1678f6b99ae881
Harshalwath <harshalwath27@gmail.com>
2026-03-18 23:16:58 +0530

    commit

--- practicequestion/code2.py
+++
@@ -1,7 +0,0 @@
-# WAP to input side of a square & print its area
-
-side = int(input("enter side of square"))
-
-area = (side*side)
-
-print(area)
---
+++ python-journey/input/code3.py
@@ -0,0 +1,7 @@
+#take temperature in celsius and convert to fahrenheit 
+
+c = int(input("Enter temperature: "))
+
+F = (c * 9/5) + 32
+
+print("In fahrenheit: " ,F)
