b3e2384266847116648cc334074ad7362db0b1eb
Harshalwath <harshalwath27@gmail.com>
2026-03-18 23:27:30 +0530

    commit

---
+++ python-journey/typcon.py/code5.py
@@ -0,0 +1,19 @@
+# ques on type conversion
+#1convert string "100" into integer & add 50
+a = int("100")
+
+print(type(a))
+
+a += 50
+
+print(a)
+
+#2 convert float 9.8 into integer.
+
+b = int(9.8)
+
+print(type(a))
+
+print(b)
+
+
