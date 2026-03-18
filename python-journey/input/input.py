71cd95d89337a9b9cc98c118e8048e091956a879
Harshalwath <harshalwath27@gmail.com>
2026-03-18 23:12:48 +0530

    commit

---
+++ python-journey/input/input.py
@@ -0,0 +1,8 @@
+#input("enter you name:")
+name = input("enter name:")
+age = int(input("enter age:")) # type casting
+marks = float(input("enter marks:")) # type casting
+
+print("welcome =", name)
+print("age =", age)
+print("marks =",marks)
