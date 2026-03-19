333f49047718600f8a72a44c9f855ad97710d9d0
Harshalwath <harshalwath27@gmail.com>
2026-03-19 19:19:26 +0530

    commit

--- python-journey/dictionary.py/pq1.py
+++ python-journey/dictionary.py/pq1.py
@@ -9,3 +9,17 @@ info = {
 info["name"] = "rahul"
 info["surname"] = "wath"
 print(info)
+
+#dict. inside dict 
+
+student ={
+    "name":"harshal wath",
+    "subject" : {
+        "phy": 98,
+        "chem": 97,
+        "math": 99 ,
+
+    }
+}
+
+print(student)
