8271a3a646903e69202298e5ace5d0b34f64a74f
Harshalwath <harshalwath27@gmail.com>
2026-03-19 19:41:02 +0530

    commit

---
+++ python-journey/dictionary.py/dictmethod.py
@@ -0,0 +1,12 @@
+student ={
+    "name":"harshal wath",
+    "subject" : {
+        "phy": 98,
+        "chem": 97,
+        "math": 99 ,
+
+    }
+}
+
+print(student.keys()) # key method
+print(len(student)) # for finding length
