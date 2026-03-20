7652ffa5d40090527034392c58abc43a011f2514
Harshalwath <harshalwath27@gmail.com>
2026-03-20 23:48:31 +0530

    commit

--- python-journey/dictionary.py/dictmethod.py
+++ python-journey/dictionary.py/dictmethod.py
@@ -15,3 +15,9 @@ print(len(student)) # for finding length
 print(list(student.values())) #value() method 
 
 print(student.items()) # it return all value in pair
+
+print(student.get("name"))
+ 
+new_dict = {"surname" : "wath"}
+student.update(new_dict)
+print(student)
