07becc2054195ef0f925f29c4791574c11e7ba26
Harshalwath <harshalwath27@gmail.com>
2026-03-20 23:12:16 +0530

    commit

---
+++ python-journey/dictionary.py/pq4.py
@@ -0,0 +1,11 @@
+#Write a program to add a new key-value pair to a dictionary.
+
+person = {
+       "name" : "harshal",
+       "age" : 21,
+       "city" : "pusad",
+
+}
+
+person["surname"] = "wath"
+print(person)
