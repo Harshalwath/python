e7c41c8de2ee4217b4d5fee4461b714a2c592b2b
Harshalwath <harshalwath27@gmail.com>
2026-03-20 22:57:28 +0530

    commit

---
+++ python-journey/dictionary.py/pq2.py
@@ -0,0 +1,10 @@
+#Create a dictionary with keys: name, age, and city. Print all values.
+
+person = {
+       "name" : "harshal",
+       "age" : 21,
+       "city" : "pusad",
+
+}
+
+print(person.values())
