9327691a45393ff69b7e058cd7a680e6764ea809
Harshalwath <harshalwath27@gmail.com>
2026-03-22 00:21:05 +0530

    commit

--- python-journey/set/setmethod.py
+++ python-journey/set/setmethod.py
@@ -4,6 +4,6 @@ collection.add(1) # set.add() add item in set.
 collection.add(2)
 collection.add(2)
 
-collection.remove(1)# set.remove item from set.
+collection.remove(1)# set.remove() remove item from set.
 
 print(collection)
