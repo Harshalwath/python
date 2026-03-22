1ad96979ee4340dbe4dc736683a59a6e0f13022c
Harshalwath <harshalwath27@gmail.com>
2026-03-22 12:55:35 +0530

    commit

---
+++ python-journey/set/pq1.py
@@ -0,0 +1,8 @@
+#You are given a list of subjects for students. Assume one classroom is required for 1 subject.how many classroom are needed by all students.
+#{"python","java","c++","python","javascript"  
+#"java","python","java","c++","c"}
+
+my_class = {"python","java","c++","python","javascript",
+            "java","python","java","c++","c"}
+
+print(len(my_class))
