f0b5fce5c0443d41c86402f59f4c83f639e6d856
Harshalwath <harshalwath27@gmail.com>
2026-04-06 23:32:00 +0530

    gender

---
+++ python-journey/conditionalstatement.py/pq10.py
@@ -0,0 +1,13 @@
+gender = str(input("enter the gender :"))
+
+if gender=="male":
+
+    print("good morning sir.")
+
+elif gender=="female":
+    print("good morning mam.")
+
+else:
+    print("good morning.")
+
+print("code end")
